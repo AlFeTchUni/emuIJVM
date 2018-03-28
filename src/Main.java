a2c62cfe0f48d89f80644b200b2c36c5b5497f70
lorenzo <lorenzo_ferron@hotmail.it>
2018-03-28 11:10:20 +0200

    setResizable(false) sul main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -31,7 +33,7 @@ public class Main extends Application {
             scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
-            // primaryStage.setResizable(false);
+            primaryStage.setResizable(false);
         } catch (Exception e) {
             e.printStackTrace();
         }
